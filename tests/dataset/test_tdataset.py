5160d90fb268264d7979a80e5a394c6ad1c57bd5
PatNicolas <patricknicolas@Patricks-MacBook-Pro-M2.local>
2024-10-11 13:52:31 -0700

    Added default dataset test

---
+++ tests/dataset/test_tdataset.py
@@ -0,0 +1,40 @@
+import unittest
+import torch
+from dataset.tdataset import TDataset
+from dataset.datasetexception import DatasetException
+
+
+class TDatasetTest(unittest.TestCase):
+
+    def test_torch_to_df(self):
+        filename = '/users/patricknicolas/dev/geometriclearning/data/MNIST/processed/training.pt'
+
+        try:
+            x = torch.tensor([
+                [[0.3, 0.6],
+                 [0.5, 1.1],
+                 [1.2, 4.3]],
+                [[3.3, 3.6],
+                 [-3.5, 5.1],
+                 [11.2, -4.3]]]
+            )
+
+            self.assertTrue(type(x) == torch.Tensor)
+            df = TDataset.torch_to_df(x)
+            print(df[0])
+        except DatasetException as e:
+            print(f'Error: {str(e)}')
+            self.assertFalse(True)
+
+    def test_torch_to_df(self):
+        filename = '/users/patricknicolas/dev/geometriclearning/data/MNIST/processed/training.pt'
+        try:
+            dfs = TDataset.torch_to_dfs(filename)
+            print(dfs[0])
+        except DatasetException as e:
+            print(f'Error: {str(e)}')
+            self.assertFalse(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
