475e017d79845b5911d51c7d9786c5aece1a1ca9
PatNicolas <patricknicolas@Patricks-MacBook-Pro-M2.local>
2025-03-04 12:53:05 -0800

    Update test for Torch and Numpy Einstein summation notation

--- tests/util/test_ein_sum.py
+++ tests/util/test_ein_sum.py
@@ -5,25 +5,65 @@ from util.ein_sum import EinSum
 
 class EinSumTest(unittest.TestCase):
 
+    @unittest.skip('Ignore')
     def test_dot(self):
+        import torch
         a = np.array([1.0, 0.5, 2.0])
         b = np.array([0.1, 2.0, 1.2])
-        ein_sum = EinSum(a, b)
-        dot_value = ein_sum.dot()
-        ref_dot_value = np.dot(a, b)
-        print(f'\nDot product Einstein sum {dot_value}\nDirect product {ref_dot_value}')
-        self.assertTrue(dot_value == ref_dot_value)
 
-    def test_matrix_mul(self):
+        # Implicit Einstein summation for dot product
+        # c = SUM (a[i].b[i]
+        implicit_notation = np.einsum('i,i', a, b)
+        # Explicit Einstein summation for dot product
+        explicit_notation = np.einsum('i,i->', a, b)
+        # Using Numpy method
+        np_dot = np.dot(a, b)
+        print(f'\nDot product Einstein notation {explicit_notation}\nNumpy dot {np_dot}')
+        self.assertTrue(explicit_notation == np_dot)
+        self.assertTrue(implicit_notation == np_dot)
+
+        # Einsum notation for PyTorch
+        einsum_notation = torch.einsum('i,i->',
+                                       torch.Tensor(a),
+                                       torch.Tensor(b))
+        torch_dot = torch.dot(torch.Tensor(a), torch.Tensor(b))
+        print(f'Einsum notation: {einsum_notation}\nTorch dot: {torch_dot}')
+
+    @unittest.skip('Ignore')
+    def test_matrix_mul_numpy(self):
         a = np.array([[1.0, 0.5], [2.0, 1.5]])
         b = np.array([[0.1, 2.0], [1.2, 0.5]])
-        ein_sum = EinSum(a, b)
-        output_matrix = ein_sum.matrix_mul()
-        ref_matrix = a @ b
-        print(f'\nMatrix multiplication Einstein:\n{output_matrix}\nDirect multiplication:\n{ref_matrix}')
-        self.assertTrue(output_matrix[0][0] == ref_matrix[0][0])
-        self.assertTrue(output_matrix[1][0] == ref_matrix[1][0])
 
+        # Explicit declared the intermediate index k
+        # c[i,j] = SUM (a[i,k].b[k,j])
+        explicit_notation = np.einsum('ij,jk->ik', a, b)
+
+        # Conventional Matrix and Numpy notation for multiplication
+        matrix_notation = a @ b
+        numpy_notation = np.matmul(a, b)
+        print(f'\nExplicit notation:\n{explicit_notation}\nMatrix notation:\n{matrix_notation}')
+        self.assertTrue(explicit_notation[0][0] == matrix_notation[0][0])
+
+    @unittest.skip('Ignore')
+    def test_matrix_mul_torch(self):
+        import torch
+        a = torch.Tensor([[1.0, 0.5, 0.4],
+                      [0.5, -0.3, 0.2],
+                      [-1.0, 0.4, -0.3]])
+        b = torch.Tensor([[0.5, -0.5, 0.0],
+                      [0.8,  0.1, -0.9],
+                      [0.6,  -0.1, 0.5]])
+
+        # Explicit declared the intermediate index k
+        # c[i,j] = SUM (a[i,k].b[k,j])
+        einsum_notation = torch.einsum('ij,jk->ik', a, b)
+
+        # Conventional torch function for multiplication
+        torch_function = torch.mm(a, b)
+        print(f'\nExplicit notation:\n{einsum_notation}\nMatrix notation:\n{torch_function}')
+        self.assertTrue(einsum_notation[0][0] == torch_function[0][0])
+
+    @unittest.skip('Ignore')
     def test_matrix_el_mul(self):
         a = np.array([[1.0, 0.5], [2.0, 1.5]])
         b = np.array([[0.1, 2.0], [1.2, 0.5]])
@@ -34,33 +74,94 @@ class EinSumTest(unittest.TestCase):
         print(f'\nMatrix el. multiplication Einstein:\n{output}\Multiplication numpy:\n{ref_output}')
         self.assertTrue(output == ref_output)
 
+    @unittest.skip('Ignore')
     def test_outer_product(self):
-        a = np.array([1.0, 0.5, 2.0, 0.7])
+        a = np.array([1.0, 0.5, 2.0, 0.7, 0.9])
         b = np.array([0.1, 2.0, 1.2])
-        ein_sum = EinSum(a, b)
-        output = ein_sum.outer_product()
-        ref_output = np.outer(a, b)
-        print(f'\nOuter product Einstein:\n{output}\nOuter product numpy:\n{ref_output}')
-        self.assertTrue(output[0][1] == ref_output[0][1])
 
+        # Explicit descriptor for outer product of two vectors
+        # C[ij] = SUM a[i].b[j]
+        explicit_notation = np.einsum('i,j -> ij', a, b)
+        numpy_function = np.outer(a, b)
+        print(f'\nOuter Product Vector Einstein:\n{explicit_notation}\nOuter product numpy:\n{numpy_function}')
+
+        a = np.array([[1.0, 0.5, 0.4],
+                      [0.5, -0.3, 0.2],
+                      [-1.0, 0.4, -0.3]])
+        b = np.array([[0.1, 2.0],
+                      [1.2, 0.5]])
+
+        # Explicit descriptor for outer product of two matrices
+        # C[ijkl] = SUM a[ij].b[kl]
+        explicit_notation = np.einsum('ij,kl -> ijkl', a, b)
+
+        # Use the Numpy outer function after reshaping
+        outer_matrix_shape = explicit_notation.shape
+        numpy_function = np.outer(a, b).reshape(outer_matrix_shape)
+        print(f'\nOuter product Matrix Einstein:\n{explicit_notation}\nOuter product numpy:\n{numpy_function}')
+
+
+    @unittest.skip('Ignore')
     def test_transpose(self):
-        a = np.array([[1.0, 0.5], [2.0, 1.5]])
-        ein_sum = EinSum(a)
-        output = ein_sum.transpose()
-        ref_output = a.T
-        print(f'\nTranspose Einstein:\n{output}\nTranspose numpy:\n{ref_output}')
-        self.assertTrue(output[0][1] == ref_output[0][1])
-        self.assertTrue(output[1][1] == ref_output[1][1])
+        from numpy import matrix
+        import torch
 
+        a = np.array([[1.0, 0.5, 0.4],
+                      [0.5, -0.3, 0.2],
+                      [-1.0, 0.4, -0.3]])
+
+        # Explicit descriptor for the transpose of a matrix
+        #  A[ji] = transpose(A[ij] )
+        np_einsum_transposed = np.einsum('ij->ji', a)
+
+        # Transpose using Numpy .T operator
+        np_transpose_operator = a.T
+
+        # Transpose using matrix transpose
+        at = a.copy()
+        matrix.transpose(at)
+        print(f'\nNumpy transpose Einsum:\n{np_einsum_transposed}\n'
+              f'Numpy Transpose operator:\n{np_transpose_operator}'
+              f'Numpy transpose function:\n{at}')
+
+        self.assertTrue(np_einsum_transposed[0][1] == np_transpose_operator[0][1])
+        self.assertTrue(np_einsum_transposed[1][1] == at[1][1])
+
+        a_tensor = torch.tensor(a)
+        torch_einsum_transposed = torch.einsum('ij->ji', a_tensor)
+        torch_transpose_operator = a_tensor.T
+        at = a.copy()
+        torch.transpose(torch.Tensor(at), 0, 1)
+        print(f'\nTorch transpose Einsum:\n{torch_einsum_transposed}\n'
+              f'Torch Transpose operator:\n{torch_transpose_operator}\n'
+              f'Torch transpose function:\n{at}')
+
+
+
+
+    @unittest.skip('Ignore')
     def test_trace(self):
-        a = np.array([[1.0, 0.5], [2.0, 1.5]])
-        ein_sum = EinSum(a)
-        output = ein_sum.trace()
-        ref_output = np.trace(a)
-        print(f'\nTrace Einstein:\n{output}\nTrace numpy:\n{ref_output}')
-        self.assertTrue(output == ref_output)
+        a = np.array([[1.0, 0.5, 0.4],
+                      [0.5, -0.3, 0.2],
+                      [-1.0, 0.4, -0.3]])
+
+        # Using the Einstein notation for trace
+        # tr(a) = SUM a[ii]
+        Einstein_notation = np.einsum('ii ->', a)
+
+        # Using the trace method
+        numpy_function = np.trace(a)
+
+        # Manual validation
+        tr = sum([a[index, index] for index in range(len(a))])
+
+        print(f'\nTrace Einstein:\n{Einstein_notation}\nTrace numpy:\n{ref_output}')
+        self.assertTrue(Einstein_notation == numpy_function)
+
 
     def test_kalman_state_eq(self):
+        import torch
+        # Components of the Kalman State-Transition equation
         A = np.array([[1.0, 0.1], [0.0, 1.0]])  # State transition matrix
         B = np.array([[0.5], [1.0]])  # Control input matrix
         x_k_1 = np.array([2.0, 1.0])  # State vector at time k-1
@@ -68,24 +169,45 @@ class EinSumTest(unittest.TestCase):
         w = np.array([0.05, 0.02])
 
         # Using numpy matrix operator
-        ref_x_k = A @ x_k_1 + B @ u + w
-        # Using Einstein summation method
-        x_k = np.einsum('ij,j->i', A, x_k_1) + np.einsum('ij,j->i', B, u) + w
-        print(f'\nEinstein state eq:\n{x_k}\nDirect state eq:\n{ref_x_k}')
-        self.assertTrue(x_k[0] == ref_x_k[0])
+        x_k_from_matrix = A @ x_k_1 + B @ u + w
 
+        # Using einsum implementation in PyTorch
+        A_k_x = torch.einsum('ij,j->i',
+                             torch.Tensor(A),
+                             torch.Tensor(x_k_1))
+        B_k_u = torch.einsum('ij,j->i',
+                             torch.Tensor(B),
+                             torch.Tensor(u))
+        x_k_from_einsum = A_k_x + B_k_u + torch.Tensor(w)
+
+
+        print(f'\nx[k] from matrix computation:\n{x_k_from_matrix}'
+              f'\nx[k] from Torch einsum:\n{x_k_from_einsum}')
+       # self.assertTrue(x_k_from_einsum[0] == x_k_from_matrix[0])
+
+
+    @unittest.skip('Ignore')
     def test_neural_linear(self):
+        import torch
         W = np.array([[0.20, 0.80, -0.50],
                       [0.50, -0.91, 0.26],
                       [-0.26, 0.27, 0.17]])  # Shape (3, 3)
         x = np.array([1, 2, 3])  # Input vector, shape (3,)
         b = np.array([2.0, 3.0, 0.5])  # Bias vector, shape (3,)
 
-        # Using @ operator
-        ref_y = W @ x + b
-        # Using Einstein summation
-        y = np.einsum('ij,j->i', W, x) + b
-        print(f'\nEinstein linear transform:\n{y}\nDirect linear transform:\n{ref_y}')
+        # Using Numpy @ matrix operator
+        y = W @ x + b
+        # Using Einstein summation notation in Numpy
+        np_einsum_y = np.einsum('ij,j->i', W, x) + b
+
+        # Using Einstein summation notation in PyTorch
+        torch_einsum_y = torch.einsum('ij,j->i',
+                                      torch.Tensor(W),
+                                      torch.Tensor(x)) + torch.Tensor(b)
+
+        print(f'\nNumpy einsum linear transform:\n{np_einsum_y}'
+              f'\nTorch einsum linear transform:\n{torch_einsum_y}'
+              f'\nDirect linear transform:\n{y}')
 
 
 
