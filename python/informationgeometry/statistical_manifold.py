Correct computation of random parameters for Poisson and Geometric distribution

python/informationgeometry/statistical_manifold.py
@@ -6,6 +6,11 @@ from geomstats.information_geometry.fisher_rao_metric import FisherRaoMetric
 from typing import Tuple, AnyStr, List
 import numpy as np
 import torch
+import os
+
+from geometry import GeometricException
+
+os.environ["GEOMSTATS_BACKEND"] = "pytorch"
 
 
 class StatisticalManifold(object):
@@ -20,7 +25,7 @@ class StatisticalManifold(object):
     - logarithm map
     """
     valid_info_manifolds = [
-        'ExponentialDistributions', 'BetaDistributions', 'PoissonDistributions', 'UnivariateNormalDistributions',
+        'ExponentialDistributions', 'BetaDistributions', 'PoissonDistributions', 'BinomialDistributions',
         'GeometricDistributions'
     ]
 
@@ -72,7 +77,11 @@ class StatisticalManifold(object):
         @return: Tensor of the random samplers
         @rtype: torch.Tensor
         """
-        return torch.Tensor(self.info_manifold.random_point(n_samples))
+        lower_bound, upper_bound = self.get_bounds()
+        delta = upper_bound - lower_bound \
+            if self.info_manifold.__class__.__name__ == 'PoissonDistributions' else 1.0
+
+        return torch.Tensor(self.info_manifold.random_point(n_samples))*delta   #torch.Tensor(self.info_manifold.random_point(n_samples))*20
 
     def metric_matrix(self, base_point: np.array = None) -> np.array:
         """
@@ -107,7 +116,19 @@ class StatisticalManifold(object):
         @return: Value of exp(v) or end point on the manifold
         @rtype: torch.Tensor
         """
-        return torch.Tensor(self.fisher_rao_metric.exp(tangent_vec.numpy(), base_point.numpy()))
+        match self.info_manifold.__class__.__name__:
+            case 'ExponentialDistributions':
+                return base_point + tangent_vec
+            case 'GeometricDistributions':
+                phi_base_point = -2.0*torch.arctanh(torch.sqrt(1.0 - base_point))
+                return 1.0 - torch.tanh(0.5*(phi_base_point+tangent_vec))**2
+            case 'PoissonDistributions':
+                return (torch.sqrt(base_point) + 0.5*tangent_vec)**2
+            case 'BinomialDistributions':
+                n = self.info_manifold.n_draws
+                return (torch.arcsin(torch.sqrt(base_point)) + 0.5 * tangent_vec/torch.sqrt(n)) ** 2
+            case _:
+                raise GeometricException(f'Exponential map for {self.info_manifold.__class__.__name__} not supported')
 
     def log(self, manifold_point: torch.Tensor, base_point: torch.Tensor) -> torch.Tensor:
         """
@@ -117,13 +138,24 @@ class StatisticalManifold(object):
             log_{\theta_{1)(\theta_{2) = v
 
         @param manifold_point: Second point on the manifold, along a geodesic.
-        @type manifold_point: tprch.Tensor
+        @type manifold_point: torch.Tensor
         @param base_point: Base point on the manifold
-        @type base_point:tprch.Tensor
-        @return: Tangent vectpr v
-        @rtype: tprch.Tensor
+        @type base_point: torch.Tensor
+        @return: Tangent vector v
+        @rtype: torch.Tensor
         """
-        return torch.Tensor(self.fisher_rao_metric.log(manifold_point.numpy(), base_point.numpy()))
+        match self.info_manifold.__class__.__name__:
+            case 'ExponentialDistributions':
+                return base_point*torch.log(manifold_point/base_point)
+            case 'GeometricDistributions':
+                return -2.0*(torch.arctanh(torch.sqrt(1.0 - manifold_point)) - torch.arctanh(torch.sqrt(1.0 - base_point)))
+            case 'PoissonDistribution':
+                return 2.0*(torch.sqrt(manifold_point) - torch.sqrt(base_point))
+            case 'BinomialDistribution':
+                n = self.info_manifold.n_draws
+                return 2.0 * torch.sqrt(n)*(torch.arcsin(torch.sqrt(manifold_point)) - torch.arcsin(torch.sqrt(base_point)))
+            case _:
+                raise GeometricException(f'Logarithm map for {self.info_manifold.__class__.__name__} not supported')
 
     """ ----------------------------  Visualization methods ----------------------   """
 
@@ -145,7 +177,7 @@ class StatisticalManifold(object):
         plt.tight_layout()
         plt.show()
 
-    def visualize_pdfs(self, parameters: List[torch.Tensor], param_label: AnyStr) -> None:
+    def visualize_pdfs(self, parameters: torch.Tensor, param_label: AnyStr) -> None:
         x = np.linspace(self.get_bounds()[0], self.get_bounds()[1], 100)
         pdfs = [self.info_manifold.point_to_pdf(params) for params in parameters]
 

tests/informationgeometry/test_statistical_manifold.py
@@ -1,15 +1,20 @@
 import unittest
 import logging
 
+import torch
 import os
+
 os.environ["GEOMSTATS_BACKEND"] = "pytorch"
 
-from geomstats.information_geometry.normal import UnivariateNormalDistributions
+from geomstats.information_geometry.binomial import BinomialDistributions
 from geomstats.information_geometry.exponential import ExponentialDistributions
-from geomstats.information_geometry.beta import BetaDistributions
+from geomstats.information_geometry.base import InformationManifoldMixin
 from geomstats.information_geometry.geometric import GeometricDistributions
-from geomstats.information_geometry.gamma import GammaDistributions
+from geomstats.information_geometry.poisson import PoissonDistributions
 from informationgeometry.statistical_manifold import StatisticalManifold
+from typing import Tuple, AnyStr
+
+
 
 class StatisticalManifoldTest(unittest.TestCase):
 
@@ -17,7 +22,7 @@ class StatisticalManifoldTest(unittest.TestCase):
     def test_init(self):
         exponential_distributions = ExponentialDistributions(equip=True)
         statistical_manifold = StatisticalManifold(exponential_distributions, (1.0, 2.0))
-        logging.info(str(statistical_manifold))
+        logging.info(f'\n{statistical_manifold=}')
         self.assertTrue(statistical_manifold.fisher_rao_metric.signature == (1, 0))
 
     @unittest.skip('ignore')
@@ -27,4 +32,87 @@ class StatisticalManifoldTest(unittest.TestCase):
         samples = statistical_manifold.samples(n_samples=4)
         logging.info('\n'.join([str(x) for x in samples]))
 
+    @unittest.skip('ignore')
+    def test_metric_matrix_exponential(self):
+        exponential_distributions = ExponentialDistributions(equip=True)
+        statistical_manifold = StatisticalManifold(exponential_distributions, (1.0, 2.0))
+        metric = statistical_manifold.metric_matrix()
+        logging.info(f'Fisher-Rao metric for exponential: {metric}')
 
+    @unittest.skip('ignore')
+    def test_metric_matrix_gamma(self):
+        gamma_distributions = GammaDistributions(equip=True)
+        statistical_manifold = StatisticalManifold(gamma_distributions, (1.0, 2.0))
+        metric = statistical_manifold.metric_matrix()
+        logging.info(f'Fisher-Rao metric for exponential: {metric}')
+
+    @unittest.skip('ignore')
+    def test_exp_map_exponential(self):
+        exponential_distributions = ExponentialDistributions(equip=False)
+        statistical_manifold = StatisticalManifold(exponential_distributions, (-2.0, 2.0))
+        tgt_vector = torch.Tensor([0.5])
+        base_point = statistical_manifold.samples(1)
+        end_point = statistical_manifold.exp(tgt_vector, base_point)
+        logging.info(end_point)
+
+    @unittest.skip('ignore')
+    def test_exp_map_geometric(self):
+        exponential_distributions = GeometricDistributions(equip=True)
+        statistical_manifold = StatisticalManifold(exponential_distributions, (1, 10))
+        tgt_vector = torch.Tensor([0.5])
+        base_point = statistical_manifold.samples(1)
+        end_point = statistical_manifold.exp(tgt_vector, base_point)
+        logging.info(end_point)
+
+    @unittest.skip('ignore')
+    def test_visualize_exponentials(self):
+        StatisticalManifoldTest.visualize(distribution=ExponentialDistributions(equip=True),
+                                          bounds=(0.0, 2.0),
+                                          num_samples=128,
+                                          param_desc="$ \\theta \in$")
+        self.assertTrue(True)
+
+
+    @unittest.skip('ignore')
+    def test_visualize_poissons(self):
+        StatisticalManifoldTest.visualize(distribution=PoissonDistributions(equip=True),
+                                          bounds=(0.0, 20.0),
+                                          num_samples=128,
+                                          param_desc='$\\lambda \in$')
+        self.assertTrue(True)
+
+    @unittest.skip('ignore')
+    def test_visualize_binomial(self):
+        n_draws = 40
+        StatisticalManifoldTest.visualize(distribution=BinomialDistributions(equip=True, n_draws=n_draws),
+                                          bounds=(0.0, n_draws/2),
+                                          num_samples=128,
+                                          param_desc='n_draws=40,  p $ \in$')
+        self.assertTrue(True)
+
+
+    # @unittest.skip('ignore')
+    def test_visualize_geometric(self):
+        StatisticalManifoldTest.visualize(distribution=GeometricDistributions(equip=True),
+                                          bounds=(1.0, 10.0),
+                                          num_samples=128,
+                                          param_desc='p $ \in$')
+        self.assertTrue(True)
+
+
+
+    @staticmethod
+    def visualize(distribution: InformationManifoldMixin,
+                  bounds: Tuple[float, float],
+                  num_samples: int,
+                  param_desc: AnyStr) -> None:
+        import torch
+
+        statistical_manifold = StatisticalManifold(distribution, bounds=bounds)
+        values = statistical_manifold.samples(num_samples)
+        min_theta = f'{float(torch.min(values)):.4f}'
+        max_theta = f'{float(torch.max(values)):.4f}'
+        statistical_manifold.visualize_pdfs(
+            values,
+            rf"{distribution.__class__.__name__} {num_samples} samples {param_desc} [{min_theta}, {max_theta}]"
+        )