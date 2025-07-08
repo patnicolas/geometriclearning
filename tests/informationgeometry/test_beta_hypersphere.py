9f8bb1aaf7753e5c5cd35852656bc23ab603f3e2
PatNicolas <patricknicolas@Patricks-MacBook-Pro-M2.local>
2025-07-07 18:52:15 -0700

    Update Unit tests for Information Geometry

--- tests/informationgeometry/test_beta_hypersphere.py
+++ tests/informationgeometry/test_beta_hypersphere.py
@@ -1,12 +1,17 @@
 import unittest
 from information_geometry.beta_hypersphere import BetaHypersphere
-
+import logging
+import python
 
 class BetaHypersphereTest(unittest.TestCase):
     def test_show_distribution(self):
-        beta_dist = BetaHypersphere()
-        num_interpolations = 10
-        num_manifold_pts = 200
-        beta_dist.show_points(num_interpolations)
-        succeeded = beta_dist.show_distribution(num_manifold_pts, num_interpolations)
-        self.assertEqual(succeeded, second=True)
+        try:
+            beta_dist = BetaHypersphere()
+            num_interpolations = 10
+            num_manifold_pts = 200
+            beta_dist.show_points(num_interpolations)
+            succeeded = beta_dist.show_distribution(num_manifold_pts, num_interpolations)
+            self.assertEqual(succeeded, second=True)
+        except AssertionError as e:
+            logging.error(e)
+            self.assertTrue(False)
